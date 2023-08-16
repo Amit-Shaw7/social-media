e4a71738099c62bc18bd05fd02c719f2686a69c8
Amit-Shaw7 <maahiamit777@gmail.com>
2023-08-16 12:49:55 +0530

    post api stabled nd pushed

--- app/controllers/posts/getPostsForExplore.js
+++ app/controllers/posts/getPostsForExplore.js
@@ -4,10 +4,10 @@ import { asyncError } from "../../utils/errors/asyncError.js";
 // Fetches posts based on createdAt for the explore page
 
 const getPostsForExplore = asyncError(async (req, res, next) => {
-    const query = req.query;
+    const {query} = req.query;
     let posts = [];
     if (query === "trending") {
-        posts = await Post.find().sort({ likes: -1 }).populate("user"); // Fetch top 10 trending posts
+        posts = await Post.find().sort({ likes: 1 }).populate("user"); // Fetch top 10 trending posts
     } else if (query === "latest") {
         posts = await Post.find().sort({ createdAt: -1 }).populate("user"); // Fetch top 10 trending posts
     } else if (query === "oldest") {
